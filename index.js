20b085ea72d0ef8bdf8708cd9c33b6ce2b6d5cd7
sfdevelopereng <thugz-roger@hotmail.com>
2024-06-06 15:31:42 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+ 
+const server = express()
+
+server.use(express.json())
+
+server.get('/hello', (req, res)=> {
+    res.json(`hello there`)
+})
+
+
+const PORT = process.env.PORT || 9000
+
+
+
+
+server.listen(PORT, () => {
+    console.log(`server on ${PORT}`)
+})
